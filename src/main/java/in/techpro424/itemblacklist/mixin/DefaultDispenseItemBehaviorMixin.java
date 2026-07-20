6c75767f33a7f29e663d35c2e331ebeabd2f0999
TechPro424 <86620085+TechPro424@users.noreply.github.com>
2026-07-20 18:59:48 +0530

    Prevent Vault from dispensing blacklisted items, and play the rejected sound instead

---
+++ src/main/java/in/techpro424/itemblacklist/mixin/DefaultDispenseItemBehaviorMixin.java
@@ -0,0 +1,22 @@
+package in.techpro424.itemblacklist.mixin;
+
+import in.techpro424.itemblacklist.config.Config;
+import in.techpro424.itemblacklist.util.Id;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Position;
+import net.minecraft.core.dispenser.DefaultDispenseItemBehavior;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(DefaultDispenseItemBehavior.class)
+public abstract class DefaultDispenseItemBehaviorMixin {
+    @Inject(method = "spawnItem", at = @At("HEAD"), cancellable = true)
+    private static void spawnItem(final Level level, final ItemStack itemStack, final int accuracy, final Direction direction, final Position position, CallbackInfo ci) {
+
+        if (Config.configIncludesId(Id.getIdFromItemStack(itemStack))) ci.cancel();
+    }
+}
---
+++ src/main/java/in/techpro424/itemblacklist/mixin/VaultStateMixin.java
@@ -0,0 +1,26 @@
+package in.techpro424.itemblacklist.mixin;
+
+import in.techpro424.itemblacklist.config.Config;
+import in.techpro424.itemblacklist.util.Id;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.vault.VaultState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(VaultState.class)
+public abstract class VaultStateMixin {
+    @Inject(method = "ejectResultItem", at = @At("HEAD"), cancellable = true)
+    private static void ejectResultItem(final ServerLevel serverLevel, final BlockPos pos, final ItemStack itemToEject, final float ejectionSoundProgress, CallbackInfo ci) {
+        if (Config.configIncludesId(Id.getIdFromItemStack(itemToEject))) {
+            serverLevel.levelEvent(3017, pos, 0);
+            serverLevel.playSound(null, pos, SoundEvents.VAULT_REJECT_REWARDED_PLAYER, SoundSource.BLOCKS);
+            ci.cancel();
+        }
+    }
+}
